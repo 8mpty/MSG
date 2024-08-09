0bac789233a37f487e11a3cdc5d9226c4bb59723
8mpty <mohdkhairin62@gmail.com>
2024-08-10 03:42:45 +0800

    checked for version group

--- build-grabber.sh
+++ build-grabber.sh
@@ -9,4 +9,34 @@ update_system() {
     sudo apt-get install -y jq
 }
 
+select_version_group() {
+    local version_group
+    version_group=$(whiptail --title "Download PaperMC Version Script" \
+        --menu "Select PaperMC Version Group:" \
+        0 0 0 \
+        "1.8" "" \
+        "1.9" "" \
+        "1.10" "" \
+        "1.11" "" \
+        "1.12" "" \
+        "1.13" "" \
+        "1.14" "" \
+        "1.15" "" \
+        "1.16" "" \
+        "1.17" "" \
+        "1.18" "" \
+        "1.19" "" \
+        "1.20" "" \
+    "1.21" "" 3>&1 1>&2 2>&3)
+    
+    if [ $? -eq 0 ]; then
+        echo "You selected version group: $version_group"
+    else
+        echo "Cancelled."
+        exit 1
+    fi
+}
+
+# Main Stuff
 update_system
+select_version_group
