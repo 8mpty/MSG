6ab3412aad2363137af9339b48a70a9a24eda053
8mpty <mohdkhairin62@gmail.com>
2024-08-10 03:39:18 +0800

    start with updating, upgrading system + instal JQ for version checking in PaperMC

--- build-grabber.sh
+++ build-grabber.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+update_system() {
+    echo "Updating and upgrading system..."
+    sudo apt update -y
+    sudo apt upgrade -y
+    
+    echo "Installing jq to check of PaperMC versions"
+    sudo apt-get install -y jq
+}
+
+update_system
